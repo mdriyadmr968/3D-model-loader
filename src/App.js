33e4837a48708e7ea10925922d57663fadaa09d5
mdriyadmr968 <mdriyadmr968@gmail.com>
2023-02-12 15:39:53 +0600

    first model loaded

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import Firstmodel from "./Components/Firstmodel/Firstmodel";
+
+function App() {
+  return (
+    <div className="App">
+      <Firstmodel></Firstmodel>
+    </div>
+  );
+}
+
+export default App;
